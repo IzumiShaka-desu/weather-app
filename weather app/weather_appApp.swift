d633bf2c221fe3546fff090ee1260be85642f0d2
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-30 18:53:30 +0700

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by Akashaka on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
